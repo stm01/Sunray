64deb22aab24b05f0f5355542e5e93b6d6995f07
AlexanderG <alexandergrau@gmx.de>
2017-03-26 21:12:43 +0200

    motor class comments

--- sunray/motor.h
+++ sunray/motor.h
@@ -4,12 +4,26 @@
 #include "pid.h"
 
 
+// selected motor
 enum MotorSelect {MOTOR_LEFT, MOTOR_RIGHT, MOTOR_MOW} ;
 typedef enum MotorSelect MotorSelect;
 
+// type of robot motion
 enum MotorMotion {MOT_PWM, MOT_LINE_TIME, MOT_LINE_DISTANCE, MOT_ROTATE_TIME, MOT_ROTATE_ANGLE, MOT_STOP, MOT_CAL_RAMP, MOT_ANGLE_DISTANCE } ;
 typedef enum MotorMotion MotorMotion;
 
+
+/* motor controller - controls motion of robot via simple commands (travel on line and angle, rotate to angle)
+
+   example usage:  
+	 
+	 Motor.begin();	 
+	 Motor.travelLineDistance(distanceCm, absoluteAngle, speedPercent);
+	 while (true){}
+	   Motor.run();		 
+	 }	
+
+*/
 
 // PWM speed:
 //    0..255  forward
@@ -21,24 +35,24 @@ class MotorClass {
     PID motorLeftPID;
     PID motorRightPID;
     PID imuPID;
-    MotorMotion motion;
+    MotorMotion motion;  // curren robot motion
 
     bool lowPass;
     bool isStucked;
-    bool paused;
+    bool paused;      // is paused?
     int  pwmMax;
     int  pwmMaxMow;
     int  rpmMax;    
     int motorLeftPWMSet;
     int motorRightPWMSet;    
 
-    int motorLeftTicks;
+    int motorLeftTicks; // left motor odometry ticks 
     int motorLeftTicksZero;
     int motorRightTicks;
     int motorRightTicksZero;
-    float ticksPerCm;
-    int ticksPerRevolution;
-    float wheelBaseCm;
+    float ticksPerCm;  // ticks per cm
+    int ticksPerRevolution; // ticks per revolution
+    float wheelBaseCm;  // wheel-to-wheel diameter
 
     float distToLine;
     float motorPosX;
@@ -56,11 +70,11 @@ class MotorClass {
     float mowerPWMCurr;
     int speedDpsSet;
 
-    float motorLeftPWMCurr;
-    float motorRightPWMCurr;
+    float motorLeftPWMCurr;  // left motor pwm
+    float motorRightPWMCurr; // right motor pwm
 
-    float motorLeftRpmCurr;
-    float motorRightRpmCurr;
+    float motorLeftRpmCurr;  // left motor rpm
+    float motorRightRpmCurr; // right motor rpm
 
     bool motorLeftSwapDir;
     bool motorRightSwapDir;
@@ -68,28 +82,40 @@ class MotorClass {
     float motorLeftEff;
     float motorRightEff;
 
-    float motorLeftSense;
-    float motorRightSense;
-    float motorMowSense;
+    float motorLeftSense; // left motor current 
+    float motorRightSense; // right  motor current 
+    float motorMowSense;  // mower motor current 
     float motorSenseMax;
     float mowSenseMax;
     
     void begin();
     void run();
     // rpm: 1.0 = max
+		/* travel for a certain distance and steer to given relative angle */
     void travelAngleDistance(int distanceCm, float angleRad, float speedRpmPerc);
+		/* travel for a certain distance and keep robot on line given by start point and angle */
     void travelLineDistance(int distanceCm, float angleRad, float speedRpmPerc);
-    void travelLineTime(int durationMS, float angleRad, float speedRpmPerc);
+    /* travel for a certain time and and keep robot on line given by start point and angle */
+		void travelLineTime(int durationMS, float angleRad, float speedRpmPerc);
+		/* rotate (pos/neg direction given by pos/neg speed) for a certain time */
     void rotateTime(int durationMS, float speedRpmPerc);
+		/* rotate to certain absolute angle */
     void rotateAngle(float angleRad, float speedRpmPerc);    
     // pwm/rpm: 1.0 = max
+		/* manually control left/right motors by given speed in percent */
     void setSpeedPWM(float leftPWMPerc, float rightPWMPerc);
-    void stopImmediately();
+    /* stop immediately (brake) */
+		void stopImmediately();
+		/* stop mower motor immediately */
     void stopMowerImmediately();
+		/* stop slowly (do not brake) */ 
   	void stopSlowly();
     void calibrateRamp();
+		/* pause current motion (robot will stop and continue with action after unpause) */
     void setPaused(bool flag);
+		/* not used */
 	  void setIsStucked();
+		/* set mower motor speed in percent */
     void setMowerPWM(float pwmPerc);
   protected:     
     unsigned long lastControlTime;	
